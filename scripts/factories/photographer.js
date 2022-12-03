2f63a1d16b579b9c973f4854edd1c5dba96331f4
jeanchaneto <jeanchaneto@gmail.com>
2022-12-03 21:08:05 +0100

    Edit photographers factory

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,8 +9,20 @@ function photographerFactory(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const infoParagraphContainer = document.createElement( 'div' );
+        infoParagraphContainer.classList.add("infoParagraphContainer");
+        const location = document.createElement( 'p' );
+        location.textContent = `${city}, ${country}`;
+        const taglineParagraph = document.createElement( 'p' );
+        taglineParagraph.textContent = tagline;
+        const dailyRate = document.createElement( 'p' );
+        dailyRate.textContent = `${price}/jour`
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(infoParagraphContainer);
+        infoParagraphContainer.appendChild(location);
+        infoParagraphContainer.appendChild(taglineParagraph);
+        infoParagraphContainer.appendChild(dailyRate);
         return (article);
     }
     return { name, picture, getUserCardDOM }
