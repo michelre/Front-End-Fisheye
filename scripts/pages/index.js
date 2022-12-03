22d86cf047d849668275395ef9bab93bfbc26b1a
jeanchaneto <jeanchaneto@gmail.com>
2022-12-03 15:38:10 +0100

    Get photographers data from JSON file

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,21 @@
     async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
+
+        const photographers = await fetch("../../data/photographers.json").then(response => {
+            if (response.ok) {
+                return response.json();
+            } throw new Error('Request failed!');
+            }, networkError => {console.log(networkError.message)}
+            ).then((jsonResponse) => {
+                return jsonResponse.photographers;
+            })
+              .catch(error => {
+                console.log(error);
+              });            
+
         return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+            photographers: [...photographers]})
+
+    };
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
@@ -36,10 +28,9 @@
     };
 
     async function init() {
-        // Récupère les datas des photographes
+        // Récupère les datas des photographes   
         const { photographers } = await getPhotographers();
         displayData(photographers);
     };
     
-    init();
-    
+init();
