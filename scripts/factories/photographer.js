0e8547f1aaddb7f002cd75841cd03b6832d5d103
jeanchaneto <jeanchaneto@gmail.com>
2022-12-03 22:04:34 +0100

    Edit front page CSS

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -13,10 +13,13 @@ function photographerFactory(data) {
         infoParagraphContainer.classList.add("infoParagraphContainer");
         const location = document.createElement( 'p' );
         location.textContent = `${city}, ${country}`;
+        location.classList.add("location");
         const taglineParagraph = document.createElement( 'p' );
         taglineParagraph.textContent = tagline;
+        taglineParagraph.classList.add("tagline");
         const dailyRate = document.createElement( 'p' );
-        dailyRate.textContent = `${price}/jour`
+        dailyRate.textContent = `${price}€/jour`;
+        dailyRate.classList.add("dailyRate");
         article.appendChild(img);
         article.appendChild(h2);
         article.appendChild(infoParagraphContainer);
