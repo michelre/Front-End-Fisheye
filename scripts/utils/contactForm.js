1d42778110a35820380452b69f41c1f9769d14ef
jeanchaneto <jeanchaneto@gmail.com>
2022-12-08 14:09:34 +0100

    Complete modal form functionality

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,3 +1,4 @@
+//Open/close Modal
 function displayModal() {
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
@@ -7,3 +8,23 @@ function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
+
+//Log modal user inputs to console
+function logUserInput() {
+    const firstNameInput = document.getElementById("First-Name").value;
+    console.log("Prénom: " + firstNameInput);
+    const lastNameInput = document.getElementById("Last-Name").value;
+    console.log("Nom: " + lastNameInput);
+    const emailInput = document.getElementById("email").value;
+    console.log("Email: " + emailInput);
+    const messageInput = document.getElementById("Your-message").value;
+    console.log("Message: " + messageInput);
+}
+
+//Modal submit button calls logUserInput
+const modalSubmitButton = document.getElementById("modal-submit-button");
+modalSubmitButton.addEventListener("click", function (e){
+    e.preventDefault();
+    logUserInput();
+    closeModal();
+});
